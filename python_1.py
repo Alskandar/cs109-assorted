e4b9cda04c8a7435796ac72745ad053552d1a9cf
Alskandar <alexander.newton.klein@gmail.com>
2018-03-23 10:47:01 -0400

    python_1.py

---
+++ python_1.py
@@ -0,0 +1,21 @@
+x=cceceea
+
+print(x)
+
+# "this text is irrelevant"
+slope = 2
+intercept = 3
+x = 1.3333
+
+y = slope*x + intercept
+
+print(y)
+
+
+
+
+
+total_monthly_rent=1200.00
+num_roommates = 4
+monthly_rent_per_person = total_monthly_rent / num_roommates
+print(monthly_rent_per_person)
